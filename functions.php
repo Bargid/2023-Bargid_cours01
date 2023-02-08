4e5010dc8bd2403b129a4c61d70d7d17446feea2
Bargid <120350488+Bargid@users.noreply.github.com>
2023-02-08 10:19:26 -0500

    image de screenshot differente

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@
 
     function ajouter_styles() {
         wp_enqueue_style('main-styles', // id de la feuille de style
-        get_template_directory_uri() . '/style.css', // 
-        array(),
-        filemtime(get_template_directory() . '/style.css'));
+            get_template_directory_uri() . '/style.css', // 
+            array(),
+            filemtime(get_template_directory() . '/style.css'));
     }
     add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
 
