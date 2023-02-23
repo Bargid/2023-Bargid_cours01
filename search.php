0640e8538fb48caba186c12b457815e9d4789c08
Bargid <120350488+Bargid@users.noreply.github.com>
2023-02-23 14:51:46 -0500

    Recherche -- Implementation d'un moteur de recherche

---
+++ search.php
@@ -0,0 +1,23 @@
+<?php
+    // Modèle search.php pour afficher les resultats de recherches
+?>
+
+<?php get_header(); ?>
+<main>
+    <code>search.php</code>
+    <section class="recherche">
+        <?php 
+            if (have_posts()) :
+                while (have_posts()) : the_post(); ?>
+                    <article>
+                        <?php // the_title('<h1>', '</h1>');
+                        // the_permalink(); ?>
+                        <h1><a href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></h1>
+    
+                        <?= wp_trim_words(get_the_excerpt(), 4); ?>
+                    </article>
+               <?php endwhile;
+            endif; ?>
+    </section>
+</main>
+<?php get_footer(); ?>
