1e982544a27561261aa4e8310e5c67bee41da322
Bargid <120350488+Bargid@users.noreply.github.com>
2023-04-18 15:29:10 -0400

    Examen_final -- Ajout de style et modification de templates

--- 404.php
+++ 404.php
@@ -8,8 +8,18 @@
     <p>Page Introuvable, Vous pouvez tenter une recherche.</p>
     <?= get_search_form(); ?>
 
-    <p class="cours-404">Nos choix de cours</p>
-    <p>Les notes de cours</p>
+    <h3>Nos choix de cours</h3>
+    <div class="cours-404"><?php
+            $leMenu = "cours-wp";
+            if (in_category('cours')) {
+                $leMenu = "cours";
+            }
+            wp_nav_menu(array(
+            "menu"      => $leMenu,
+            "container" => "nav"
+        )); ?></div>
+
+    <h3>Les notes de cours</h3>
     <div class="notes-404"><?php
             $leMenu = "note-wp";
             if (in_category('cours')) {
