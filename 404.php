a8027ac0b3ec0ed4911a4b046d216fafbfac0e4d
Bargid <120350488+Bargid@users.noreply.github.com>
2023-04-18 14:11:30 -0400

    Examen_final -- Instanciantion initial de la branche

--- 404.php
+++ 404.php
@@ -6,6 +6,6 @@
 <main class="site__main">
     <h3>index.php</h3>
     <code>404.php</code>
-    <h1>Erreur 404, la page n'existe pas (Gateem!)</h1>
+    <h1>Erreur 404, la page n'existe pas!</h1>
 </main>
 <?php get_footer(); ?>
